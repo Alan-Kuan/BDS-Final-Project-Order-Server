233e4fd7f9a79ce4660c6e6c67d2e3ddde4ba900
Alan-Kuan <kuanhaoan@gmail.com>
2023-12-17 14:39:47 +0800

    chore: update openapi schema

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
+
 from modules.specs import Order
 from modules.db import insert_order
 
@@ -8,3 +10,18 @@ app = FastAPI()
 async def create_order(order: Order):
     insert_order(order)
     return order
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+
+    openapi_schema = get_openapi(
+        title='Smartphone Cover Order',
+        version='1.0.0',
+        routes=app.routes
+    )
+
+    app.openapi_schema = openapi_schema
+    return openapi_schema
+
+app.openapi = custom_openapi
