8abc0337ce2d04a09ea2f9d79ea1e5570bbb270b
Alan-Kuan <kuanhaoan@gmail.com>
2023-12-19 22:57:07 +0800

    feat: add dev/prod modes

--- main.py
+++ main.py
@@ -1,27 +1,52 @@
 from fastapi import FastAPI
 from fastapi.openapi.utils import get_openapi
+import sys
+import uvicorn
 
 from modules.specs import Order
 from modules.db import insert_order
 
-app = FastAPI()
+if __name__ == '__main__':
+    if len(sys.argv) == 1 or len(sys.argv) > 3:
+        print(f'Usage: {sys.argv[0]} <dev|prod> [domain name]')
+        exit(1)
 
-@app.post("/order")
-async def create_order(order: Order):
-    insert_order(order)
-    return order
+    if sys.argv[1] == 'dev':
+        port = 8000
+        host = '127.0.0.1'
+    elif sys.argv[1] == 'prod':
+        port = 80
+        host = '0.0.0.0'
+    else:
+        print(f'Usage: {sys.argv[0]} <dev|prod> [domain name]')
+        exit(1)
 
-def custom_openapi():
-    if app.openapi_schema:
-        return app.openapi_schema
+    app = FastAPI()
 
-    openapi_schema = get_openapi(
-        title='Smartphone Cover Order',
-        version='1.0.0',
-        routes=app.routes
-    )
+    @app.post("/order")
+    async def create_order(order: Order):
+        insert_order(order)
+        return order
 
-    app.openapi_schema = openapi_schema
-    return openapi_schema
+    def custom_openapi():
+        if app.openapi_schema:
+            return app.openapi_schema
 
-app.openapi = custom_openapi
+        openapi_schema = get_openapi(
+            title='Smartphone Cover Order',
+            version='1.0.0',
+            routes=app.routes
+        )
+        if sys.argv[1] == 'prod' and sys.argv[2]:
+            openapi_schema['servers'] = [
+                {
+                    'url': sys.argv[2],
+                    'description': 'production server'
+                },
+            ]
+
+        app.openapi_schema = openapi_schema
+        return openapi_schema
+
+    app.openapi = custom_openapi
+    uvicorn.run(app, port=port, host=host)
